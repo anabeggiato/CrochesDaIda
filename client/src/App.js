bce3a80dfe864e2df924afd2d759eca0f3f8af8b
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-07-12 00:22:06 -0300

    feat: remove unused components and assets to streamline the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Products from './pages/products';
 import Admin from './pages/admin';
-import Home from './pages/home'
-import Product from './pages/product';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={ <Home /> } />
-          <Route path='/produtos' element={ <Products /> } />
-          <Route path='/produto/:id' element={ <Product /> } />
+          <Route path='/' element={ <Products /> } />
           <Route path='/admin' element={ <Admin /> } />
         </Routes>
       </Router>
