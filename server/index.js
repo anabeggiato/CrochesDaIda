58df8e3f3094105a60c920c1b868f58bc3679ae3
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-07-11 21:52:28 -0300

    feat: remove home router and associated events model

--- server/index.js
+++ server/index.js
@@ -14,9 +14,6 @@ app.use('/produtos', productsRouter)
 const adminRouter = require('./routes/admin')
 app.use('/admin', adminRouter)
 
-const homeRouter = require('./routes/home')
-app.use('/', homeRouter)
-
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log('server running on port 3001');
