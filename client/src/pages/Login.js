b5f4eb9d7a4396aadf2b5ba24920c682eb697100
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-07-27 22:05:00 -0300

    feat: Update login API URL to use environment variable for better configuration

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,7 +15,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:3001/auth/login', {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, {
         username,
         password
       });
